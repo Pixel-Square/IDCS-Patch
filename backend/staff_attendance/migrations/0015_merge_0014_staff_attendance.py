3712f4c8d7a11dc954dc0c3d31fdaa904758ca64
AbiVarsanP <website2k5@gmail.com>
2026-03-27 17:11:40 +0530

    merged hari

---
+++ backend/staff_attendance/migrations/0015_merge_0014_staff_attendance.py
@@ -0,0 +1,14 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        (
+            'staff_attendance',
+            '0014_rename_staff_biome_user_id_16fc4d_idx_staff_biome_user_id_04f2e6_idx_and_more',
+        ),
+        ('staff_attendance', '0014_staff_time_limit_override'),
+    ]
+
+    operations = []
